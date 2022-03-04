1194772c74ddad28e02e8905cce775b4870d4c95
cbl <lennart.carbe@gmail.com>
2022-03-04 19:26:14 +0100

    wip

---
+++ README.md
@@ -0,0 +1,17 @@
+# Admin Laravel Package
+
+## Setup
+
+Install the package via composer
+
+```shell
+composer require macramejs/admin-laravel
+```
+
+Create an admin application:
+
+```shell
+php artisan make:admin admin
+```
+
+And follow the given instructions.
--- src/Foundation/Console/MakeAdminCommand.php
+++ src/Foundation/Console/MakeAdminCommand.php
@@ -41,6 +41,10 @@ class MakeAdminCommand extends BaseMakeCommand
         $this->line('3. Run the migrations and the seeder:');
         $this->info('php artisan migrate:fresh --seed');
 
+        $this->line('4. Visit '.url($this->name()).' and login using the following credentials:');
+        $this->line('username: admin@admin.com');
+        $this->line('password: secret');
+
         return 0;
     }
 

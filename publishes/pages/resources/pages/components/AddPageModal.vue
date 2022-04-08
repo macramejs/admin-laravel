ec3993b5a6ff6c7f63f241c8ce987494b8ff9bc6
Jannes Behrens <jannescb@me.com>
2022-04-08 12:20:10 +0200

    Fix Add page modal

--- publishes/pages/resources/pages/components/AddPageModal.vue
+++ publishes/pages/resources/pages/components/AddPageModal.vue
@@ -1,24 +1,42 @@
 <template>
-    <Modal :open="true" v-bind="$attrs" md>
-        <h2>New Page</h2>
-        <div>
+    <Button
+        round
+        dark
+        @click="isOpen = true"
+    >
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="-4.5 -4.5 24 24"
+            class="w-4 h-4 fill-white"
+        >
+            <path
+                d="M8.9 6.9v-5a1 1 0 1 0-2 0v5h-5a1 1 0 1 0 0 2h5v5a1 1 0 1 0 2 0v-5h5a1 1 0 1 0 0-2h-5z"
+            ></path>
+        </svg>
+    </Button>
+    <Modal lg v-model:open="isOpen" title="New Page">
+        <div class="space-y-3 ">
             <Input label="Name" v-model="form.name" />
             <Select
                 label="Template"
                 :options="templateOptions"
                 v-model="form.template"
+
             />
-            {{ templateOptions }}
         </div>
-        <Button @click="submit"> Save </Button>
+        <template v-slot:footer>
+            <Button @click="submit"> Save </Button>
+        </template>
     </Modal>
 </template>
 
 <script lang="ts" setup>
-import { defineEmits } from 'vue';
+import { defineEmits, ref } from 'vue';
 import { Modal, Input, Select, Button } from '@macramejs/admin-vue3';
 import { useForm } from '@macramejs/macrame-vue3';
 import { templateOptions } from './content/templates';
+
+const isOpen = ref<boolean>(false);
 
 const emit = defineEmits(['pageAdded', 'close']);
 

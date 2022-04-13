3967a38b49a54fec8fbcaa5ec46ec733a1b21784
Jannes Behrens <jannescb@me.com>
2022-04-13 14:39:06 +0200

    Add Image and grid in select image modal

--- publishes/pages/resources/pages/components/content/sections/SelectImageModal.vue
+++ publishes/pages/resources/pages/components/content/sections/SelectImageModal.vue
@@ -1,19 +1,22 @@
 <template>
     <Button @click="isOpen = true">Select Image</Button>
     <Modal v-model:open="isOpen" @close="isOpen = false">
-        <div
-            v-for="(image, key) in mediaIndex.items"
-            :key="key"
-            class="w-8 h-8 bg-red"
-            @click="emit('update:modelValue', image)"
-        >
-            {{ image }}
+        <div class="grid grid-cols-12 gap-4">
+
+            <div
+                v-for="(image, key) in mediaIndex.items"
+                :key="key"
+                class="col-span-6 sm:col-span-4 md:col-span-4 lg:col-span-2 flex items-center cursor-pointer"
+                @click="selectImage(image)"
+            >
+                <img :src="image.url" />
+            </div>
         </div>
     </Modal>
 </template>
 
 <script lang="ts" setup>
-import { Card, Button } from '@macramejs/admin-vue3';
+import { Button } from '@macramejs/admin-vue3';
 import { Modal } from '@macramejs/admin-vue3';
 import { defineEmits, PropType, ref } from 'vue';
 import { mediaIndex } from '@admin/modules/media';
@@ -31,4 +34,9 @@ const props = defineProps({
 });
 
 mediaIndex.loadItems();
+
+const selectImage = (image) => {
+    emit('update:modelValue', image)
+    isOpen.value = false;
+}
 </script>

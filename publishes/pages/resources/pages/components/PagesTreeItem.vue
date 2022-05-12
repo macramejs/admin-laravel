5e0aed1be3f5852cc8715f96441264074e6a70a4
Chrissle28 <christian.z.dev@gmail.com>
2022-05-12 11:43:14 +0200

    add context menu to pages tree item

--- publishes/pages/resources/pages/components/PagesTreeItem.vue
+++ publishes/pages/resources/pages/components/PagesTreeItem.vue
@@ -7,7 +7,44 @@
             {{ page.name }}
         </Link>
 
-        <ContextButton />
+        <ContextMenu placement="right" custom-button>
+            <template #button>
+                <ContextButton />
+            </template>
+            <div class="flex flex-col">
+                <button
+                    class="w-full py-2.5 px-[14px] text-left hover:bg-white hover:bg-opacity-20 rounded-[8px]"
+                >
+                    Edit
+                </button>
+                <button
+                    class="w-full py-2.5 px-[14px] text-left hover:bg-white hover:bg-opacity-20 rounded-[8px]"
+                >
+                    Duplicate
+                </button>
+                <div class="bg-white w-full my-1.5 h-px bg-opacity-20"></div>
+                <button
+                    class="w-full flex items-center text-red-signal gap-2 py-2.5 px-[14px] text-left hover:bg-red-signal hover:bg-opacity-20 rounded-[8px]"
+                >
+                    <svg
+                        width="24"
+                        height="24"
+                        class="origin-left scale-75"
+                        stroke-width="1.5"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M19 11v9.4a.6.6 0 0 1-.6.6H5.6a.6.6 0 0 1-.6-.6V11M10 17v-6M14 17v-6M21 7h-5M3 7h5m0 0V3.6a.6.6 0 0 1 .6-.6h6.8a.6.6 0 0 1 .6.6V7M8 7h8"
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                    </svg>
+                    <span>Delete</span>
+                </button>
+            </div>
+        </ContextMenu>
 
         <template v-slot:disclosure>
             <PagesTree :tree="children" />
@@ -18,7 +55,7 @@
 <script lang="ts" setup>
 import { Tree } from '@macramejs/macrame-vue3';
 import { {{ model }} } from '@{{ app }}/types';
-import { TreeItem, ContextButton } from '@macramejs/admin-vue3';
+import { TreeItem, ContextButton, ContextMenu } from '@macramejs/admin-vue3';
 import { Link } from '@inertiajs/inertia-vue3';
 import PagesTree from './PagesTree.vue';
 import { computed, PropType } from 'vue';

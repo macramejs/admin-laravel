761c575983cb6d46be5513d9f38daa4a260abf75
cbl <lennart.carbe@gmail.com>
2022-05-13 14:55:43 +0200

    wip

--- src/Pages/Console/MakePageBuilderCommand.php
+++ src/Pages/Console/MakePageBuilderCommand.php
@@ -133,15 +133,16 @@ class MakePageBuilderCommand extends BaseMakeCommand
 
         $route = $this->route();
         $insert = "
-    // {$route}
-    Route::get('/{$route}', [{$model}Controller::class, 'index'])->name('{$route}.index');
-    Route::get('/{$route}/items', [{$model}Controller::class, 'items'])->name('{$route}.items');
-    Route::get('/{$route}/{page}', [{$model}Controller::class, 'show'])->name('{$route}.show');
-    Route::post('/{$route}', [{$model}Controller::class, 'store'])->name('{$route}.store');
-    Route::post('/{$route}/order', [{$model}Controller::class, 'order'])->name('{$route}.order');
-    Route::post('/{$route}/{page}', [{$model}Controller::class, 'update'])->name('{$route}.update');
-    Route::post('/{$route}/{page}/meta', [{$model}Controller::class, 'meta'])->name('{$route}.meta');
-    Route::post('/{$route}/{page}/upload', [{$model}Controller::class, 'upload'])->name('{$route}.upload');";
+    // pages
+    Route::get('/pages', [PageController::class, 'index'])->name('pages.index');
+    Route::get('/pages/items', [PageController::class, 'items'])->name('pages.items');
+    Route::get('/pages/{page}/{tab?}', [PageController::class, 'show'])->name('pages.show');
+    Route::delete('/pages/{page}', [PageController::class, 'destroy'])->name('pages.destroy');
+    Route::post('/pages', [PageController::class, 'store'])->name('pages.store');
+    Route::post('/pages/order', [PageController::class, 'order'])->name('pages.order');
+    Route::put('/pages/{page}', [PageController::class, 'update'])->name('pages.update');
+    Route::post('/pages/{page}/meta', [PageController::class, 'meta'])->name('pages.meta');
+    Route::post('/pages/{page}/upload', [PageController::class, 'upload'])->name('pages.upload');";
         $before = '});';
 
         $routesPath = base_path('routes/'.$this->app().'.php');

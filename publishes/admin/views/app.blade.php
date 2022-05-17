8aedf074f429af14ecefb28eceeb9a1946b0a1b3
Lukas Heller <36259611+lpheller@users.noreply.github.com>
2022-05-17 11:26:49 +0200

    add csrf

--- publishes/admin/views/app.blade.php
+++ publishes/admin/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <link href="{{ asset('/css/{{ name }}/app.css') }}" rel="stylesheet">
         <script src="{{ asset('/js/{{ name }}/app.js') }}" defer></script>
+        @csrf
     </head>
     <body>
         @inertia

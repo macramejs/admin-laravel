2f3b30fe30f444183081318a0a718ecc46dde3ff
Lukas Heller <36259611+lpheller@users.noreply.github.com>
2022-05-17 15:23:45 +0200

    update icon

--- publishes/media/resources/pages/components/SelectionMenu.vue
+++ publishes/media/resources/pages/components/SelectionMenu.vue
@@ -20,21 +20,7 @@
             @click="selection.delete"
         >
             <template #icon>
-                <svg
-                    width="24"
-                    height="24"
-                    class="origin-left scale-75"
-                    stroke-width="1.5"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        d="M19 11v9.4a.6.6 0 0 1-.6.6H5.6a.6.6 0 0 1-.6-.6V11M10 17v-6M14 17v-6M21 7h-5M3 7h5m0 0V3.6a.6.6 0 0 1 .6-.6h6.8a.6.6 0 0 1 .6.6V7M8 7h8"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
-                </svg>
+                <IconMoreHorizontal class="origin-left scale-75" />
             </template>
             <span>Delete</span>
         </ContextMenuItem>
@@ -46,6 +32,7 @@ import {
     ContextMenu,
     ContextMenuItem,
     ContextMenuDivider,
+    IconMoreHorizontal,
 } from '@macramejs/admin-vue3';
 import { PropType } from 'vue';
 import { Selection } from '../modules';

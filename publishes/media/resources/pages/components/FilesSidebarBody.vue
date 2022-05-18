01c79197d36ee0f277e1f8a453ec06831b167a50
Lukas Heller <36259611+lpheller@users.noreply.github.com>
2022-05-18 15:14:55 +0200

    update files sidebar for new color and icons

--- publishes/media/resources/pages/components/FilesSidebarBody.vue
+++ publishes/media/resources/pages/components/FilesSidebarBody.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="flex flex-col gap-1 p-2 border-b border-gray-400">
+    <div class="flex flex-col gap-1 p-2 border-b border-gray-400 text-gray-100">
         <button
             :class="
                 mediaIndex.filters.types.value.includes('images')
                     ? 'bg-gray-100'
                     : ''
             "
-            class="px-4 py-2 mb-1 rounded-[8px] transition-colors w-full text-left duration-300 hover:bg-gray-100"
+            class="px-4 py-2 mb-1 rounded-[8px] transition-colors w-full text-left duration-300 hover:bg-gray-500 hover:bg-opacity-20"
             @click="mediaIndex.filters.types.toggle('images')"
         >
             Images
@@ -17,71 +17,30 @@
                     ? 'bg-gray-100'
                     : ''
             "
-            class="px-4 py-2 mb-1 rounded-[8px] transition-colors w-full text-left duration-300 hover:bg-gray-100"
+            class="px-4 py-2 mb-1 rounded-[8px] transition-colors w-full text-left duration-300 hover:bg-gray-500 hover:bg-opacity-20"
             @click="mediaIndex.filters.types.toggle('documents')"
         >
             Documents
         </button>
     </div>
-    <div class="flex flex-col gap-1 p-2 border-b border-gray-400">
+    <div class="flex flex-col gap-1 p-2 border-b border-gray-400 text-gray-100">
         <div class="flex items-center gap-3 px-5 pt-16 pb-6 -mx-2">
-            <svg
-                width="24"
-                stroke-width="1"
-                height="24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                    d="M5 19.5V5a2 2 0 0 1 2-2h11.4a.6.6 0 0 1 .6.6V21M9 7h6M6.5 15H19M6.5 18H19M6.5 21H19"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                />
-                <path
-                    d="M6.5 18c-1 0-1.5-.672-1.5-1.5S5.5 15 6.5 15M6.5 21c-1 0-1.5-.672-1.5-1.5S5.5 18 6.5 18"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                />
-            </svg>
+            <IconBookStack />
             <span class="inline-block text-xl"> Collections </span>
-            <button
-                class="inline-flex items-center justify-center ml-auto text-white rounded-full h-9 w-9 bg-gradient-to-r from-indigo-900 focus:outline-none focus:ring-4 focus:ring-orange-100 active:from-indigo-500 active:to-indigo-500 to-indigo-900 hover:from-gradient-red-500 hover:to-gradient-orange-500"
-                @click="showForm = !showForm"
-            >
-                <svg
-                    width="24"
-                    stroke-width="1"
-                    height="24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        d="M5 19.5V5a2 2 0 0 1 2-2h11.4a.6.6 0 0 1 .6.6V21M9 7h6M6.5 15H19M6.5 18H19M6.5 21H19"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                    />
-                    <path
-                        d="M6.5 18c-1 0-1.5-.672-1.5-1.5S5.5 15 6.5 15M6.5 21c-1 0-1.5-.672-1.5-1.5S5.5 18 6.5 18"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
-                </svg>
-            </button>
+            <ButtonRound white sm @click="showForm = !showForm" class="ml-auto">
+                <IconPlus class="w-4 h-4"></IconPlus>
+            </ButtonRound>
         </div>
         <AddCollectionForm class="mb-4" v-if="showForm" />
     </div>
-    <div class="flex flex-col gap-1 p-2 border-b border-gray-400">
+    <div class="flex flex-col gap-1 p-2 border-b border-gray-400 text-gray-100">
         <Link
             v-for="c in collections"
             :key="c.id"
-            :class="c.id == collection?.id ? 'bg-gray-100' : ''"
-            class="px-4 py-2 rounded-[8px] transition-colors text-left duration-300 hover:bg-gray-100"
+            :class="c.id == collection?.id ? 'bg-gray-50 bg-opacity-10' : ''"
+            class="px-4 py-2 rounded-[8px] transition-colors text-left duration-300 hover:bg-gray-100 hover:bg-gray-50 hover:bg-opacity-20"
             :href="
-                c.id == collection?.id
-                    ? `/admin/media`
-                    : `/admin/media/${c.id}`
+                c.id == collection?.id ? `/admin/media` : `/admin/media/${c.id}`
             "
         >
             <div class="flex justify-between">
@@ -93,7 +52,12 @@
 </template>
 
 <script lang="ts" setup>
-import { Input } from '@macramejs/admin-vue3';
+import {
+    Input,
+    IconBookStack,
+    ButtonRound,
+    IconPlus,
+} from '@macramejs/admin-vue3';
 import { Link } from '@inertiajs/inertia-vue3';
 import { RadioGroup, RadioGroupOption } from '@headlessui/vue';
 import { PropType, ref, computed } from 'vue';

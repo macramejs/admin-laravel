d784dbb205c84777765a6b9ce1bff585ba764e9c
Lukas Heller <36259611+lpheller@users.noreply.github.com>
2022-05-18 10:37:24 +0200

    replace request with inertia visit

--- publishes/pages/resources/modules/page/index.ts
+++ publishes/pages/resources/modules/page/index.ts
@@ -1,9 +1,6 @@
 import { Inertia } from '@inertiajs/inertia';
 import { Page } from '@admin/types/resources';
-import { del } from '../request';
 
 export const deletePage = async (page: Page) => {
-    await del(`/admin/pages/${page.id}`);
-
-    Inertia.visit(`/admin/pages`);
+    Inertia.delete(`/admin/pages/${page.id}`);
 };

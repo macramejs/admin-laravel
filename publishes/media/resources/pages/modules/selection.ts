ab465fc421dcb1199834b7045017de49f6304633
Lukas Heller <36259611+lpheller@users.noreply.github.com>
2022-05-20 16:09:33 +0200

    fix media index reload after delete

--- publishes/media/resources/pages/modules/selection.ts
+++ publishes/media/resources/pages/modules/selection.ts
@@ -1,32 +1,52 @@
 import { Media, MediaCollection } from '@admin/types';
 import { Inertia } from '@inertiajs/inertia';
-import { reactive, ref, watch } from 'vue'
-import { post } from '@admin/modules/request';
+import { reactive } from 'vue';
+import { mediaIndex } from '@admin/modules/media';
 
 export interface Selection {
-    files: Media[],
-    addToCollection: (collection: MediaCollection) => void,
-    delete: () => void
+    files: Media[];
+    addToCollection: (collection: MediaCollection) => void;
+    delete: () => void;
 }
 
-const useSelection = function(files: []) {
-    const sel = reactive<Selection>({ files, addToCollection() {}, delete() {} });
+const useSelection = function (files: []) {
+    const sel = reactive<Selection>({
+        files,
+        addToCollection() {},
+        delete() {},
+    });
 
-    sel.addToCollection = (collection) => {
-        if(sel.files.length == 0) {
+    sel.addToCollection = collection => {
+        if (sel.files.length == 0) {
             return;
         }
 
-        return Inertia.post(`/admin/media/${collection.id}/add`, { 
-            ids: sel.files.map((file) => file.id)
-        });
-    }
+        return Inertia.post(
+            `/admin/media/${collection.id}/add`,
+            {
+                ids: sel.files.map(file => file.id),
+            },
+            {
+                onSuccess() {
+                    sel.files = [];
+                },
+            }
+        );
+    };
 
     sel.delete = () => {
-        return Inertia.post(`/admin/media/delete`, { 
-            ids: sel.files.map((file) => file.id)
-        });
-    }
+        return Inertia.post(
+            `/admin/media/delete`,
+            {
+                ids: sel.files.map(file => file.id),
+            },
+            {
+                onSuccess() {
+                    mediaIndex.reload();
+                },
+            }
+        );
+    };
 
     return sel;
 };

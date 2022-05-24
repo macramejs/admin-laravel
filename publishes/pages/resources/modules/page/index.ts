bdc3b014b0816410fbf2345c0ac0bc26f0330516
Lukas Heller <36259611+lpheller@users.noreply.github.com>
2022-05-24 15:43:12 +0200

    ask for confirmation when deleting page

--- publishes/pages/resources/modules/page/index.ts
+++ publishes/pages/resources/modules/page/index.ts
@@ -2,5 +2,7 @@ import { Inertia } from '@inertiajs/inertia';
 import { Page } from '@admin/types/resources';
 
 export const deletePage = async (page: Page) => {
-    Inertia.delete(`/admin/pages/${page.id}`);
+    if (confirm(`Are you sure you want to delete Page ${page.name}?`)) {
+        Inertia.delete(`/admin/pages/${page.id}`);
+    }
 };

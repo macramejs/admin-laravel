4c791509cbaeb4f844521909c4c43186e9fb7f8b
Lukas Heller <36259611+lpheller@users.noreply.github.com>
2022-05-24 12:01:39 +0200

    fix naming

--- publishes/admin/app/Http/Requests/LoginRequest.php
+++ publishes/admin/app/Http/Requests/LoginRequest.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
 
-class LoginRequets extends FormRequest
+class LoginRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.

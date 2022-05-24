dfa2638e5c2c5adc2551df2355dcfb5837992697
Lukas Heller <36259611+lpheller@users.noreply.github.com>
2022-05-24 15:43:37 +0200

    improve slugify for ßäüö

--- publishes/pages/resources/modules/helpers/index.ts
+++ publishes/pages/resources/modules/helpers/index.ts
@@ -1,7 +1,11 @@
-export const slugify = (str) => {
+export const slugify = str => {
     return str
         .toString()
         .toLowerCase()
+        .replace(/ä/g, 'ae')
+        .replace(/ö/g, 'oe')
+        .replace(/ü/g, 'ue')
+        .replace(/ß/g, 'ss')
         .replace(/\s+/g, '-') // Replace spaces with -
         .replace(/[^\w\-]+/g, '') // Remove all non-word chars
         .replace(/\-\-+/g, '-') // Replace multiple - with single -

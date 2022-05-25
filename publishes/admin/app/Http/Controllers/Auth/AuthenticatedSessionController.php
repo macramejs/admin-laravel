1e73a537b04405abea9997ba447611c5e3f80249
Lukas Heller <36259611+lpheller@users.noreply.github.com>
2022-05-25 14:39:26 +0200

    fix logout methods

--- publishes/admin/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ publishes/admin/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -55,12 +55,13 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request)
     {
-        Auth::guard('{{ name }}')->logout();
+        Auth::guard('web')->logout();
 
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/admin/login');
+
     }
 }

d1da182b9fe98518b5403a18187c0ac44ae4c1c5
cbl <lennart.carbe@gmail.com>
2022-05-31 11:51:13 +0200

    wip

--- publishes/media/app/models/{{ file_model }}.php
+++ publishes/media/app/models/{{ file_model }}.php
@@ -7,26 +7,58 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Macrame\Admin\Media\Contracts\AttachableFile;
 use Macrame\Admin\Media\Traits\IsAttachableFile;
 
-class {{ file_model }} extends Model implements AttachableFile
+class File extends Model implements AttachableFile
 {
     use IsAttachableFile;
 
+    /**
+     * Attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'display_name', 'group', 'disk', 'filepath', 'filename', 'mimetype',
         'size', 'meta',
     ];
 
+    /**
+     * Attribute casts.
+     *
+     * @var array
+     */
     protected $casts = [
         'size' => 'int',
         'meta' => 'json',
     ];
 
+    /**
+     * Default attributes.
+     *
+     * @var array
+     */
     protected $attributes = [
         'disk' => 'public',
     ];
 
+    /**
+     * Related collections.
+     *
+     * @return BelongsToMany
+     */
     public function collections(): BelongsToMany
     {
-        return $this->attached({{ page }}Collection::class);
+        return $this->attached(MediaCollection::class);
+    }
+
+    /**
+     * Gets the url to the file.
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->storage()->url(
+            'c/'.$this->getFilepath().DIRECTORY_SEPARATOR.$this->filename
+        );
     }
 }

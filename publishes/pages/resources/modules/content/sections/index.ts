49b55440c033e692d2beb671b01908951ccaf8c5
Jannes Behrens <jannes@MacBook-Pro-von-Jannes.fritz.box>
2022-06-06 14:46:46 +0200

    remove redun

--- publishes/pages/resources/modules/content/sections/index.ts
+++ publishes/pages/resources/modules/content/sections/index.ts
@@ -7,27 +7,3 @@ export { default as SectionCards } from './SectionCards.vue';
 export { default as SectionAccordion } from './SectionAccordion.vue';
 export { default as SectionLogoWall } from './SectionLogoWall.vue';
 export { default as SectionCarousel } from './SectionCarousel.vue';
-
-import SectionTextFull from './SectionTextFull.vue';
-import SectionTextImage from './SectionTextImage.vue';
-import SectionInfoBox from './SectionInfoBox.vue';
-import SectionImageFull from './SectionImageFull.vue';
-import SectionGallery from './SectionGallery.vue';
-import SectionCards from './SectionCards.vue';
-import SectionAccordion from './SectionAccordion.vue';
-import SectionLogoWall from './SectionLogoWall.vue';
-import SectionCarousel from './SectionCarousel.vue';
-
-const sections = {
-    text_full: SectionTextFull,
-    image_full: SectionImageFull,
-    carousel: SectionCarousel,
-    gallery: SectionGallery,
-    info_box: SectionInfoBox,
-    cards: SectionCards,
-    text_image: SectionTextImage,
-    accordion: SectionAccordion,
-    logo_wall: SectionLogoWall,
-};
-
-export { sections };

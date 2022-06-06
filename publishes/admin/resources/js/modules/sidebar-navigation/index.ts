377e31168210dbd79e7227fe3f1398dc902f2c96
Jannes Behrens <jannes@MacBook-Pro-von-Jannes.fritz.box>
2022-06-06 20:15:53 +0200

    fix sidebarnavlink typing

--- publishes/admin/resources/js/modules/sidebar-navigation/index.ts
+++ publishes/admin/resources/js/modules/sidebar-navigation/index.ts
@@ -1,9 +1,10 @@
+import { Component } from 'vue';
 // import { IconDashboard } from '@macramejs/admin-vue3';
 
 type SidebarNavigationLink = {
     title: string;
     href: string;
-    icon?: string;
+    icon?: string|Component;
 };
 
 export const sidebarLinks = <SidebarNavigationLink[]>[
